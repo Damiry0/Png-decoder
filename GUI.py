f6c8ef72f0b3fd2c2f3602e631e2874d7d81ba8d
Damian Ryś <damian.rys0@gmail.com>
2022-05-03 13:57:51 +0300

    Upgraded gui for anonymization functionality

--- GUI.py
+++ GUI.py
@@ -19,28 +19,35 @@ def draw_figure(canvas, figure):
 
 List_combo = []
 # define the window layout
-layout = [[sg.Text("Choose a file: "), sg.FileBrowse(key="-IN-")],
-          [sg.Canvas(key='-CANVAS-'), sg.Multiline("", size=(15, 15), key='OUTPUT', visible=False, no_scrollbar=True)],
+layout = [[sg.Text("Choose a file: "),
+           sg.FileBrowse(key="-IN-")],
+
+          [sg.Canvas(key='-CANVAS-'),
+           sg.Multiline("", size=(20, 15), key='OUTPUT', visible=False, no_scrollbar=True)],
+
           [sg.Button('Decode'),
-           sg.InputCombo(List_combo, key='chunk_names_combo', size=(5, 5), visible=False,
-                         enable_events=True)]]
+           sg.InputCombo(List_combo, key='chunk_names_combo', size=(5, 5), visible=False, enable_events=True)],
+
+          [sg.InputText(key='File to Save', default_text='filename', enable_events=True),
+           sg.InputText(key='Save As', do_not_clear=False, enable_events=True, visible=False),
+           sg.FileSaveAs(initial_folder='/tmp', )]]
 
 # create the form and show it without the plot
 window = sg.Window('Png-decoder', layout, finalize=True,
                    element_justification='center', font='Helvetica 18')
 
-fig, chunk_names, Width, Height, Bit_depth, Color_type, Gamma, SRGB, PHYs, CHRM, TIME, TEXT, Compression_method, Filter_method, Interlace_method = object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object()
+fig, chunk_names, Width, Height, Bit_depth, Color_type, Gamma, SRGB, PHYs, CHRM, TIME, TEXT, Compression_method, Filter_method, Interlace_method, anomizated_chunks = object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object(), object()
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Exit":
         break
     elif event == "Decode":
-        fig, chunk_names, Width, Height, Bit_depth, Color_type, Gamma, SRGB, PHYs, CHRM, TIME, TEXT, Compression_method, Filter_method, Interlace_method = png.open_png(
+        fig, chunk_names, Width, Height, Bit_depth, Color_type, Gamma, SRGB, PHYs, CHRM, TIME, TEXT, Compression_method, Filter_method, Interlace_method, anomizated_chunks = png.open_png(
             values["-IN-"])
         fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
         window['OUTPUT'].update(
-            f"Width:{Width} Height:{Height} Bit_depth:{Bit_depth} Color_type:{Color_type} Gamma:{Gamma} cHRM:{CHRM} sRGB:{SRGB} pHYs:{PHYs} date:{PHYs} text:{TEXT}",
+            f"Width:{Width}px Height:{Height}px Bit_depth:{Bit_depth} Color_type:{Color_type} Gamma:{Gamma} cHRM:{CHRM} sRGB:{SRGB} pHYs:{PHYs} date:{PHYs} text:{TEXT}",
             visible=True)
         List_combo = chunk_names
         window['chunk_names_combo'].update(values=List_combo, visible=True)
@@ -67,15 +74,20 @@ while True:
         elif values['chunk_names_combo'] == b"IDAT":
             window['OUTPUT'].update(f"IDAT chunk loaded correctly, results are shown on the left", visible=True)
 
-        # elif values['chunk_names_combo'] == b"tIME":
-        #     window['OUTPUT'].update(
-        #         f"Width:{Width} Height:{Height} Bit_depth:{Bit_depth} Color_type:{Color_type} Gamma:{Gamma} cHRM:{CHRM} sRGB:{SRGB} pHYs:{PHYs} date:{PHYs} text:{TEXT}",
-        #         visible=True)
+        elif values['chunk_names_combo'] == b"tIME":
+            window['OUTPUT'].update(f"tIME:{TIME}", visible=True)
 
         elif values['chunk_names_combo'] == b"tEXt":
-            window['OUTPUT'].update(f"text:{TEXT}", visible=True)
+            window['OUTPUT'].update(f"\n".join("{}\t{}".format(k, v) for k, v in TEXT.items()), visible=True)
 
         else:
-            window['OUTPUT'].update(f"error")
+            window['OUTPUT'].update(f"error, chunk not supported")
+    elif event == 'Save As':
+        filename = values['Save As']
+        if filename:
+            window['File to Save'].update(value=filename)
+            f = open(filename, "wb")
+            f.write(anomizated_chunks)
+            f.close()
 
 window.close()
